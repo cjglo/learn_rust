c449824dbdb334d61a023fee11af37948eecb654
Christopher Gallo <cjg100@georgetown.edu>
2020-09-09 23:57:56 -0400

    Finished section, moved to primitive types

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-// Quick little program to practice some new syntax for if statements and match statements
+// Long file covering Rust-from-C++ Git Lessons, broken into units
 
 
 fn main() 
 {
+
+    // Control FLOW:
 let person1_age = 23;
 let person2_age = 45;
 
@@ -55,7 +57,39 @@ fn bouncer(person: i32)
 
 bouncer(person1_age);
 
+// Ends Controll Flow section
 
+println!("\n\n\n\n");
+
+// Begins: Primitive Data Types & operations:
+
+let x: bool = true;
+// As above, you decalre var type with : and type after
+// u and i are short hand for unsigned and signed varaibles
+let y = 34i8; // unsigned 34
+let z: u8 = 34u8; // 34 unsgined 8 declared after as well
+let alpha = 34f64; // compiler infers type
+
+println!("{}, {}, {}, {}", x, y, z, alpha);
+
+// Key Note: "let" lets you redeclare variables, which is very helpful since they are immutable by default
+let b = 34;
+println!("b: {}", b);
+let b: bool = false;
+println!("b: {} \n", b);
+
+
+// You can also use binary, octal, hex, : using 0b 0o 0x
+// You can also use _ and they will be ignored
+
+let x = 0b1010;
+let y = 0o_15;
+let z = 0xff;
+
+println!("{}, {}, {}", x, y, z);
+
+// Strings and Chars are unicode in Rust, so slightly different then C++
+// Will come back to this later
 
 
 
