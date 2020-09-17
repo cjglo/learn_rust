f0bac9d5cc85d0900f673771747c0177487678bc
Christopher Gallo <cjg100@georgetown.edu>
2020-09-17 13:20:00 -0400

    working more on first book project

---
+++ quick_projects/target/debug/deps/rand-40838e6bc99e84fc.d
@@ -0,0 +1,9 @@
+/home/chrisjg/GitHub/learn_rust/quick_projects/target/debug/deps/rand-40838e6bc99e84fc.rmeta: /home/chrisjg/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs /home/chrisjg/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs /home/chrisjg/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs
+
+/home/chrisjg/GitHub/learn_rust/quick_projects/target/debug/deps/librand-40838e6bc99e84fc.rlib: /home/chrisjg/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs /home/chrisjg/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs /home/chrisjg/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs
+
+/home/chrisjg/GitHub/learn_rust/quick_projects/target/debug/deps/rand-40838e6bc99e84fc.d: /home/chrisjg/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs /home/chrisjg/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs /home/chrisjg/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs
+
+/home/chrisjg/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/lib.rs:
+/home/chrisjg/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/distributions/mod.rs:
+/home/chrisjg/.cargo/registry/src/github.com-1ecc6299db9ec823/rand-0.3.23/src/rand_impls.rs:
