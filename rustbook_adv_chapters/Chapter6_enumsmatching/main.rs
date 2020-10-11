336f31952ddbb452e0a811b1febe13be93be4721
Christopher Gallo <cjg100@georgetown.edu>
2020-10-10 22:32:18 -0400

    Working through chapter 6

--- rustbook_adv_chapters/Chapter6_enumsmatching/main.rs
+++ rustbook_adv_chapters/Chapter6_enumsmatching/main.rs
@@ -4,25 +4,53 @@ fn main()
 {
 
     
-    enum IpAddrType
+    enum IpAddrType // Can create enum and attatch another type with a ()
     {
-        V4,
-        V6,
+        V4(IpAddr),
+        V6(IpAddr),
     }
     
-    struct IpAddr // Could add this to record address, but actually there is a more efficient way
+    struct IpAddr // Defined the above is possible
     {
-        kind: IpAddrType,
-        address: String,
+        //something
     }
-
-    let home = IpAddr {
-        kind: IpAddrType::V4,
-        address: String::from("127.0.0.1"),
-    };
 
     
 
 
+
+    // Can even have varying types with lots of diff characteristics
+
+    enum Message // This has a noral type, a anonymous struct with two intgers, a type with a string, and a type with a tuple
+    { // The real advatage of this rather then building each as own struct: We can now build functions easily that would take any of these as parameters
+        Quit,
+        Move(x: i32, y: i32),
+        Write(String),
+        ChangeColor(i32, i32, i32),
+    }
+    
+
+
+
+    // Can also define impls on our enums!
+    impl Message 
+    {
+        fn call(&self)
+        {
+            // method body......
+        }
+    }
+
+
+
+
+
+
+
+
+
+    // Option Starts here ------------------------------------------ very helpful
+
+    
 
 }
