ace9d401abbe8912814e693d787332c2bb8e57c3
chris <chris.j.gallo3@gmail.com>
2021-01-12 16:52:33 -0500

    Working on traits, examples and excerises

--- rustbook_adv_chapters/Traits/main.rs
+++ rustbook_adv_chapters/Traits/main.rs
@@ -1,8 +1,9 @@
+// use std::io;
 
 fn main()
 {
 	// just practicing traits
-
+	// impl for already defined types:
 	trait Describe
 	{
 		fn describe(&self) -> &str;
@@ -16,13 +17,139 @@ fn main()
 		}
 	}
 
-
 	impl Describe for u64
 	{
 		fn describe(&self) -> &str
 		{
 		"this is a unsigned 64 bit"
 		}
+	}
+
+	let myType: bool = true;
+
+	println!("What computer says: {}", myType.describe());
+
+
+	// Next practice ---------------- :
+
+	trait Client
+	{
+		// new
+		fn new(name: String) -> Self; // note: Capital (S)elf means implementor type
+
+		// membership to stdout
+		fn display_mem(&self) -> ();
+
+		// testing points
+		fn eligible_for_reward(&self) -> bool;
+	}
+
+
+	struct SilverClient
+	{
+		name: String,
+	}
+
+	struct GoldClient
+	{
+		name: String,
+		points: u8,
+	}
+
+	struct PlatClient
+	{
+		name: String,
+		points: u8,
+		location: String,
+	}
+
+	impl PlatClient
+	{
+		fn set_location(&mut self, location: String)
+		{
+			self.location = location;
+		}
+	}
+
+	impl Client for PlatClient
+	{
+		// new
+		fn new(name: String) -> PlatClient
+		{
+			PlatClient
+			{
+				name,
+				points: 0,
+				location: String::from("undefined"),
+			}
+		}
+
+		// membership to stdout
+		fn display_mem(&self) -> ()
+		{
+			println!("This is a Platinum Client!");
+		}
+
+		// testing points
+		fn eligible_for_reward(&self) -> bool
+		{
+			self.points >= 100
+		}
+
+	}
+
+
+	impl Client for SilverClient
+	{
+		// new
+		fn new(name: String) -> SilverClient
+		{
+			SilverClient
+			{
+				name,
+			}
+		}
+
+		// membership to stdout
+		fn display_mem(&self) -> ()
+		{
+			println!("This is a Silver Client!");
+		}
+
+		// testing points
+		fn eligible_for_reward(&self) -> bool
+		{
+			false
+		}
+		
+	}
+
+
+	
+	impl Client for GoldClient
+	{
+		// new
+		fn new(name: String) -> GoldClient
+		{
+			GoldClient
+			{
+				name,
+				points: 0,
+			}
+		}
+
+		// membership to stdout
+		fn display_mem(&self) -> ()
+		{
+			println!("This is a Gold Client!");
+		}
+
+		// testing points
+		fn eligible_for_reward(&self) -> bool
+		{
+			self.points >= 150
+		}
+		
 	}
 
 
