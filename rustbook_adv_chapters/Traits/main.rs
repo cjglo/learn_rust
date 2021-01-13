9b0c23f2a48970af0d2db99137ecdec804924969
chris <chris.j.gallo3@gmail.com>
2021-01-13 18:32:46 -0500

    Working through traits (chap. 10), almost done.

--- rustbook_adv_chapters/Traits/main.rs
+++ rustbook_adv_chapters/Traits/main.rs
@@ -152,6 +152,55 @@ fn main()
 		
 	}
 
+	let cl1 = SilverClient::new(String::from("John"));
+	let cl2 = GoldClient::new(String::from("Sam"));
+	let cl3 = PlatClient::new(String::from("Beth"));
 
+
+	cl1.display_mem();
+	// println!("It is {} that this member has enough points", cl1.eligible_for_reward());
+
+	cl2.display_mem();
+	// println!("It is {} that this member has enough points", cl2.eligible_for_reward());
+
+	cl3.display_mem();
+	// println!("It is {} that this member has enough points", cl3.eligible_for_reward());
+
+
+	// Traits as parameters
+
+	pub fn notify(person: impl Client) // short-hand
+	{
+		println!("It is {} that this member has enough points", person.eligible_for_reward());
+	}
+
+	notify(cl2);
+	notify(cl3);
+
+	// if we wanted to force many parameters to be the same type, must use the long-hand:
+	pub fn compareSameType<T: Client>(person1: T, person2: T)
+	{
+		println!("Just made this for example...");
+	}
+
+	// Can require multiple traits like this:
+	pub fn anotherExample(item: impl Client + Clone) // or anotherExample<T: Client + Describe>( etc. )
+	{
+		println!("Also example...");
+	}
+
+	// For many trait bounds, there is a useful short-hand:
+	// The WHERE syntax
+
+	pub fn some_func<T, U>(t: T, u: U) -> ()
+		where T: Describe,
+			  U: Clone + Client,
+	{
+		println!("Still does nothign");
+	}
+
+
+	// can also return types with traits
+	// cont next time (pafe 188)....
 
 }
