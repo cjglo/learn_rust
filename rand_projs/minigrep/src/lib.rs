0898cb3752d502791739c979a39643cd80561562
chris <chris.j.gallo3@gmail.com>
2021-02-14 19:05:05 -0500

    mroe work on Chapter 12: grep command line project.

--- rand_projs/minigrep/src/lib.rs
+++ rand_projs/minigrep/src/lib.rs
@@ -13,6 +13,11 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+pub fn search_case_insensitive<'a>(_query: &str, _contents: &'a str) -> Vec<&'a str> {
+
+    vec![]
+}
+
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 
     let mut vec = vec![];
@@ -63,17 +68,31 @@ mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
 
         assert_eq!(
             vec!["safe, fast, productive."],
             search(query, contents)
         );
+    }
+
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUst";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(vec!["Rust:", "Trust me."], search_case_insensitive(query, contents));
     }
 
 }
