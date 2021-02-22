aaaaa6478c1488b4e953deb5c80213c8b6b3e997
chris <chris.j.gallo3@gmail.com>
2021-02-21 22:38:56 -0500

    Started iterator section of Chapter 13.

---
+++ rustbook_chapters/iters_closures/iters.rs
@@ -0,0 +1,9 @@
+// Chapter 13 Iterators
+
+// Just need to impl Item type and next() to impl full Iterator trait
+
+// BIG NOTE: Rust's ownership rules means that many methods completely consume iterators
+// ex/ vec has a sum method that sums its elements, this takes ownership and destorys vec
+
+
+
