d90a2628ffbb6bebc6d8e9172e35cbcee2b5356f
cjglo <chris.j.gallo3@gmail.com>
2021-04-04 17:41:04 -0400

    Finished section on Box Smart Pointer.

--- rustbook_chapters/SmartPointers/main.rs
+++ rustbook_chapters/SmartPointers/main.rs
@@ -1,6 +1,7 @@
 // building a cons list to use Box pointer in way that is relevant for
 
 use crate::List::{Cons, Nil};
+use std::ops::Deref;
 
 
 enum List {
@@ -15,7 +16,30 @@ fn main() {
             Box::new(Cons(3,
                 Box::new(Nil))))));
 
+    // * The defer trait is how you can have a type treated as a ref (overloads * oeprator)
+    // * Here is a self made Box (except without heap alloc)
+    struct MyBox<T>(T);
+
+    impl<T> MyBox<T> {
+        fn new(x: T) -> MyBox<T> {
+            MyBox(x)
+        }
+    }
+
+    impl<T> Deref for MyBox<T> {
+        type Target = T;
+
+        fn deref(&self) -> &T {
+            &self.0
+        }
+    }
+
+    let x = 5;
+    let y = MyBox::new(x);
+   
+    println!("{} \n", *y);
+
     
 
-
+    
 }
