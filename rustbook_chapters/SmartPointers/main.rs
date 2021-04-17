4b5cb3a2df9877709ad610717429db09605f5e54
cjglo <chris.j.gallo3@gmail.com>
2021-04-16 22:54:27 -0400

    Worked through Rc<T> and RefCell<T> sections.

--- rustbook_chapters/SmartPointers/main.rs
+++ rustbook_chapters/SmartPointers/main.rs
@@ -4,11 +4,13 @@ use crate::List::{Cons, Nil};
 use std::ops::Deref;
 use std::rc::Rc;
 
+use std::cell::RefCell;
 
-// enum List {
-//     Cons(i32, Box<List>),
-//     Nil,
-// }
+#[derive(Debug)]
+enum List {
+    Cons(Rc<RefCell<i32>>, Rc<List>), // change Rc Ref to Rc to Box and back for different parts of code
+    Nil,
+}
 
 fn main() {
 
@@ -59,22 +61,75 @@ fn main() {
         }
     } 
 
-    // * Broke up the rest of the section into functions for each area
+    // The rest of the section is in functions
     
+    // ref_counted_smart_ptr();
+
+     
+
+    // * NOTE: Interior mutability is accomplished through RefCell<T> 
+    // * This allows to have borrow rules checked at runtime 
+    // * This is used for when compiler can't tell code is safe, so this allows 
+    // * us to pass immatuble variables but have them be mutable.
+    // * but you still MUST follow the borrow rules, otherwise run time crash
+
+
+    // ! using Rc<RefCell<T>> together lets you have multiple owners and mutaute!
+    // ! it is very cool because the RefCell keeps track of references and protects us from data races!
+    println!("\n\n");
+    example_rc_ref();
+
+
+//
 }
 
 
 
 fn ref_counted_smart_ptr() {
 
-    // * For when a single value may have multiple ownership
-    enum List {
-        Cons(i32, Rc<List>),
-        Nil
-    }
+    // ! Commented out because needs List to be i32, rc<List>
 
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    let b = Cons(3, Rc::clone(&a));
-    let c = Cons(4, Rc::clone(&a)); 
+    // // * For when a single value may have multiple ownership
+    // enum List {
+    //     Cons(i32, Rc<List>),
+    //     Nil
+    // }
+
+    // let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    // let b = Cons(3, Rc::clone(&a));
+    // let c = Cons(4, Rc::clone(&a)); 
+
+    // /* 
+    // Rc clone increases reference count, so a will only be cleaned up
+    // references to it are out of scope
+
+    // Important to note: Rc::clone is NOT a deep copy
+    // */
+    // println!("Strong count of references:{}", Rc::strong_count(&a));
+    // {
+    //     let d =  Cons(5, Rc::clone(&a));
+    //     println!("Strong count of references:{}", Rc::strong_count(&a));
+    // }
+    
+
+
+}
+
+
+fn example_rc_ref() {
+    let value = Rc::new(RefCell::new(5));
+
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+    let b = Cons(Rc::new(RefCell::new(6)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10; // recursive deref allows one * to cycle until hit 5, this is from previous section
+
+    println!("a after: {:?}", a);
+    println!("b after: {:?}", b);
+
+    // ! HUGE NOTE: IT IS POSSIBLE TO MEMORY LEAK WITH THIS!!!! 
+    // ! if you have refernces to eachother, then ref counter never hits 0 and so
+    // ! memory is NEVER cleaned! So be careful with Rc<RefCell<T>>!!!!
 
 }
