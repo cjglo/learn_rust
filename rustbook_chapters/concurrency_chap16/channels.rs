330e1c75e8787f88bd6df14f50627ffbbc875bcc
cjglo <chris.j.gallo3@gmail.com>
2021-07-06 23:50:56 -0400

    more concurrency work

--- rustbook_chapters/concurrency_chap16/channels.rs
+++ rustbook_chapters/concurrency_chap16/channels.rs
@@ -1,14 +1,32 @@
 use std::thread;
 use std::sync::mpsc;
+use std::time::Duration;
+
 
 fn main() {
     let (tx, rx) = mpsc::channel();
 
+    // * we can clone the sending channel to have multiple senders 
+    let tx1 = mpsc::Sender::clone(&tx);
+
     thread::spawn(move || {
         let val = String::from("hi");
-        // tx.send(val).unwrap();
+        tx.send(val).unwrap();
     });
 
-    let recieved: String = rx.recv().unwrap();
-    println!("Got: {}", recieved);
+    thread::spawn(move || {
+        let val1 = vec!["Hello", "Ferris"];
+
+        for val in val1 {
+            tx1.send(val.to_string()).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for recieved in rx {
+        println!("Got: {}", recieved);
+    }
+
+    // * We create two threads that send data to the main thread 
+    // * Main thread doesn't end because the for loop will continue until all senders are closed
 }
