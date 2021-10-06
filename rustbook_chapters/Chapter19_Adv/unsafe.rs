0805712dd87cbeb36e45ac63b76993ff67a16189
cjglo <chris.j.gallo3@gmail.com>
2021-10-05 19:46:33 -0700

    moving into chapter 19

---
+++ rustbook_chapters/Chapter19_Adv/unsafe.rs
@@ -0,0 +1,20 @@
+
+
+
+fn main()  {
+
+	// four "powers" given by unsafe:
+
+
+	// 1. Raw Pointers
+		// Can use raw pointers, which don't follow borrow rules or non-null rule
+		// Must clean up
+
+	// 2. Calling an unsafe function
+		// unsafe functions are esentially like unsafe blocks, by calling them we
+		// are saying we have read the docs and know how to use it properly
+
+	// 3.  
+
+
+}
